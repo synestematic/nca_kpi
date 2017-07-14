91ddcabb81ecbe0256395bf60a01f5242038b0d9
Fed Rizzo <federico.rizzo@auto1.com>
2017-07-14 13:07:55 +0200

    - Adds new CSS assets for human application layout - Installs jQuery 3.2.1

--- nca_dj/static/js/main.js
+++ nca_dj/static/js/main.js
@@ -1,3 +1,28 @@
-window.onload = function() {
+// window.onload = function() {
     // alert('Benvenuto');
-}
+// }
+$(function() {
+	"use strict";
+
+	$('.user_row').on('click', function() {
+		// $(this).css('background-color', '#000000');
+		alert('new');
+	});
+
+	// $('#my_form').on('submit', function(event) {
+	// 	event.preventDefault();
+	// 	alert('Prevents form to to default action');
+	// });
+
+});
+
+
+
+
+
+
+
+//  function bla() {
+// 	console.log('ciao mondo')
+// 	return 1
+// }
